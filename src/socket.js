8a8d6275b146064dc479a41d52f26b5e7605025a
Akshatsainiaks <akshatsaini336@gmail.com>
2025-11-16 20:08:31 +0530

    A

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:4000";
+const URL = import.meta.env.VITE_SOCKET_URL || "https://uppay-backend.onrender.com";
+
 
 const socket = io(URL, {
   transports: ["websocket"],
